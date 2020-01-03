8118d05addb89bd348c28d16f872ea6a16a5a4a5
vsparrow.com <khan@vsparrow.com>
2020-01-03 22:07:44 +0000

    Add model blog for use with mongoDB:

---
+++ models/blog.js
@@ -0,0 +1,19 @@
+//for use once we have setup mongoDB
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+	title: {type: String, required: true, minlength: 5},
+	author: String,
+	url: String,
+	like: Number
+})
+
+blogSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id 
+		delete returnedObject.__v		
+	}
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
