7314ee4fc70478555657b88faeb117f5cdc4ea1a
vsparrow.com <khan@vsparrow.com>
2020-01-04 19:08:09 +0000

    setup testing

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 
 let PORT = process.env.PORT || 3003
 let MONGODB_URI = process.env.MONGODB_URI
-let TEST_MONGODB_URI = process.env.TEST_MONGODB_URI
 
-module.exports = {PORT, MONGODB_URI, TEST_MONGODB_URI}
+if(process.env.NODE_ENV === 'test'){
+	MONGODB_URI = process.env.TEST_MONGODB_URI	
+}
+
+module.exports = {PORT, MONGODB_URI}
