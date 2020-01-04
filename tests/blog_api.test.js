a38b445d739a633b565d42f784e1a25e38fc255d
vsparrow.com <khan@vsparrow.com>
2020-01-04 20:00:44 +0000

    update tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-	await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+beforeEach(async ()=> {
+	await Blog.deleteMany({})	
 })
+
+describe("GET BLOGS", () => {
+	
+	test('notes are returned as json', async () => {
+		const result = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+		// console.log(result.body)
+	})
+
+	test('returns the correct amount of blogs', async () => {
+		const results = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+		expect(results.body.length).toBe(0)
+	})	
+})
+
+
 
 afterAll(() => mongoose.connection.close())
