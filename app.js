afef559c88d9316d70e7b7a36649069d60baf832
vsparrow.com <khan@vsparrow.com>
2020-01-04 17:01:05 +0000

    Add mongoDB connection

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
 
-// ********************************************************** app.use start
+// ********************************************************** Mongoose connection
+console.log('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {console.log('connected to MongoDB')})
+.catch(error => console.log('error connecting to MongoDB:', error.message))
+
+// ********************************************************** app.use start.
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
