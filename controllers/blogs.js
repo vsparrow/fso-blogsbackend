563cc821541fc5028b603040e27f607180f63bd7
vsparrow.com <khan@vsparrow.com>
2020-01-04 18:32:10 +0000

    removed unneeded variable

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,12 +2,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 // ********************************************************** variables and helper functions
-const blogs = [      {_id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0}]
 
 // ********************************************************** ROUTING start
 blogsRouter.get('/', async (request, response, next) => {  
