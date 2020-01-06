486116c8c497d4fdd5b7ad418b53cdda548f7704
vsparrow.com <khan@vsparrow.com>
2020-01-06 22:21:06 +0000

    Add POST /api/users

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,23 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
 	const result = await User.find({})
 	response.json(result)
+})
+
+//username password, name
+usersRouter.post('/', async (request, response, next) => {
+	try {
+		const body = request.body
+		const saltRounds = 10
+		const passwordHash = await bcrypt.hash(body.password, saltRounds)
+		const user = new User({username: body.username, name: body.name, passwordHash})
+		const savedUser = await user.save()
+		response.json(savedUser)
+	} catch(exception) { next(exception) }
 })
 
 module.exports = usersRouter
