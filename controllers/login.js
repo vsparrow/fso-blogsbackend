2bcc4a160feaf2138d87fb0fe19cc823c458139a
vsparrow.com <khan@vsparrow.com>
2020-01-09 04:59:03 +0000

    add POST LOGIN tests, fighting with jest over doublequotes

--- controllers/login.js
+++ controllers/login.js
@@ -3,19 +3,25 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter.post('/', async (request, response) => {
+loginRouter.post('/', async (request, response, next) => {
 	const body = request.body
-	
-	const user = await User.findOne({username: body.username})
-	const passwordCorrect = user === null
-	? false
-	: await bcrypt.compare(body.password, user.passwordHash)
-	
-	if(!(user && passwordCorrect)){return response.status(401).json({error: 'invalid username or password'})}
-	
-	const userForToken = {username: user.username, id: user._id}
-	const token = jwt.sign(userForToken, process.env.SECRET)
-	response.status(200).send({token, username: user.username, name: user.name})
+	try{
+		const user = await User.findOne({username: body.username})
+		// console.log("body.password is", body.password)
+		// console.log("user.passwordHash", user.passwordHash)
+		const passwordCorrect = user === null
+		? false
+		: await bcrypt.compare(body.password, user.passwordHash)
+		// let passwordCorrect = false
+		// if(user !== null){ passwordCorrect = await bcrypt.compare(body.password, user.passwordHash)}
+		
+		if(!(user && passwordCorrect)){return response.status(401).json({error: 'invalid username or password'})}
+
+		const userForToken = {username: user.username, id: user._id}
+		const token = jwt.sign(userForToken, process.env.SECRET)
+		response.status(200).send({token, username: user.username, name: user.name})		
+	} catch(exception) { next(exception) }
+
 })
 
 module.exports = loginRouter
---
+++ tests/login_api.test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+// const Blog = require('../models/blog') 
+const User = require('../models/user')
+const helper = require("./test_helper")
+const api = supertest(app)
+const url = '/api/login'
+
+beforeEach(async ()=> {
+	await User.deleteMany({})
+	//we post rather than save user directly else would have to create passwordHash in the test. 
+	// If we made changes to how we encrpt it would not reflect automatically here
+	// await new User(await helper.singleUser()).save() //<-bad
+	await api.post('/api/users').send(await helper.singleUser())
+})
+
+describe('POST Login', ()=>{
+	test('successful login returns a token', async ()=>{
+		const user = await helper.singleUser()
+		const userData = {username: user.username, password: user.password}
+		const result = await api.post(url).send(userData).expect(200).expect("Content-Type", /application\/json/)
+		expect(result.body).toHaveProperty("token")
+	})
+})	
+
+afterAll(() => mongoose.connection.close())
